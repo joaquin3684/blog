bc6cc9f234b7e2d2503d7238633c20e8e37c1d85
LucasBlanco <lucas_manuel_blanco@hotmail.com>
2017-08-16 14:55:29 -0300

    Agrego las funcionalidades de editar y eleminar de abm_comercializador

--- public/js/controladores/ABM_comercializadores.js
+++ public/js/controladores/ABM_comercializadores.js
@@ -8,7 +8,7 @@ app.controller('ABM_comercializador', function($scope, $http, $compile, $sce, Ng
     var data = {
       'nombre': $scope.nombreComerc,
       'apellido': $scope.apellidoComerc,
-    'dni': $scope.documentoComerc,
+      'dni': $scope.documentoComerc,
       'cuit': $scope.cuitComerc,
       'domicilio': $scope.domicilioComerc,
       'telefono': $scope.telefonoComerc,
@@ -16,28 +16,17 @@ app.controller('ABM_comercializador', function($scope, $http, $compile, $sce, Ng
       'password': $scope.contraseniaComerc,
       'email': $scope.emailComerc
     };
-    console.log(data);
+
     return $http({
       url: 'abm_comercializador',
       method: 'post',
-      data: {
-        'nombre': $scope.nombreComerc,
-        'apellido': $scope.apellidoComerc,
-      'dni': $scope.documentoComerc,
-        'cuit': $scope.cuitComerc,
-        'domicilio': $scope.domicilioComerc,
-        'telefono': $scope.telefonoComerc,
-        'usuario': $scope.usuarioComerc,
-        'password': $scope.contraseniaComerc,
-        'email': $scope.emailComerc
-      },
+      data: data,
 
     }).then(function successCallback(response) {
       $scope.traerElementos();
     }, function errorCallback(response) {
 
     });
-    console.log("Marta")
 
   }
 
@@ -50,7 +39,7 @@ app.controller('ABM_comercializador', function($scope, $http, $compile, $sce, Ng
         if (typeof response.data === 'string') {
           return [];
         } else {
-            console.log(response);
+            console.log(response.data);
             $scope.datosabm = response.data;
             $scope.paramsABMS = new NgTableParams({
               page: 1,
@@ -72,8 +61,57 @@ app.controller('ABM_comercializador', function($scope, $http, $compile, $sce, Ng
 
   $scope.traerElementos();
 
-  $scope.traerElemento = function() {
-    console.log("algo");
+  $scope.traerElemento = function(id) {
+
+    return $http({
+      url: 'abm_comercializador/'+ id,
+      method: 'get',
+      // data: data,
+    }).then(function successCallback(response) {
+      $scope.abmConsultado = response.data;
+    }, function errorCallback(response) {
+
+    });
+  }
+
+  $scope.editarFormulario = function (id) {
+
+    var data = {
+      'nombre': $scope.abmConsultado.nombre,
+      'apellido': $scope.abmConsultado.apellido,
+      'dni': $scope.abmConsultado.documento,
+      'cuit': $scope.abmConsultado.cuit,
+      'domicilio': $scope.abmConsultado.domicilio,
+      'telefono': $scope.abmConsultado.telefono,
+      'usuario': $scope.abmConsultado.usuario,
+      'email': $scope.abmConsultado.email
+    };
+
+    return $http({
+      url: 'abm_comercializador/'+ id,
+      method: 'put',
+      data: data,
+    }).then(function successCallback(response) {
+      $scope.traerElementos();
+      console.log("Exito al editar");
+      $('#editar').modal('toggle');
+    }, function errorCallback(response) {
+
+    });
+
+  }
+
+  $scope.borrarElemento = function (id) {
+
+    return $http({
+      url: 'abm_comercializador/'+ id,
+      method: 'delete',
+    }).then(function successCallback(response) {
+      $scope.traerElementos();
+      console.log("Exito al eliminar");
+    }, function errorCallback(response) {
+
+    });
   }
 
 
--- resources/views/ABM_comercializadores.blade.php
+++ resources/views/ABM_comercializadores.blade.php
@@ -65,7 +65,7 @@
                       <label class="control-label col-md-3 col-sm-3 col-xs-12" for="apellido">Apellido<span class="required">*</span>
                       </label>
                       <div class="col-md-6 col-sm-6 col-xs-12">
-                        <input type="number" step="0.01" id="apellido" name="apellido" class="form-control col-md-7 col-xs-12" placeholder="Ingrese el apellido del comercializador" ng-model="apellidoComerc">{[{errores.cuota_social[0]}]}
+                        <input type="text" id="apellido" name="apellido" class="form-control col-md-7 col-xs-12" placeholder="Ingrese el apellido del comercializador" ng-model="apellidoComerc">{[{errores.cuota_social[0]}]}
                       </div>
                     </div>
 
@@ -177,7 +177,7 @@
                                     <div id="exportTable">
                                       @verbatim
                                         <table id="tablita" ng-table="paramsABMS" class="table table-hover table-bordered">
-                                      
+
                                             <tbody data-ng-repeat="abm in $data" data-ng-switch on="dayDataCollapse[$index]" >
                                             <tr class="clickableRow" title="Datos">
                                                 <td title="'Nombre'" sortable="'nombre'">
@@ -206,8 +206,8 @@
                                                 </td>
 
                                                 <td id="botones">
-                                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editar" ng-click="traerElemento()"><span class="glyphicon glyphicon-pencil"></span></button>
-                                                <button type="button" class="btn btn-danger" ng-click="enviarFormulario('Borrar', abm.id)"><span class="glyphicon glyphicon-remove"></span></button>
+                                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editar" ng-click="traerElemento(abm.id)"><span class="glyphicon glyphicon-pencil"></span></button>
+                                                <button type="button" class="btn btn-danger" ng-click="borrarElemento(abm.id)"><span class="glyphicon glyphicon-remove"></span></button>
                                                 </td>
                                             </tr>
                                         </table>
@@ -238,25 +238,26 @@
   <div class="modal-dialog">
 
     <!-- Modal content-->
+    @verbatim
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h4 class="modal-title">Editar</h4>
       </div>
       <div class="modal-body">
-         <form class="form-horizontal form-label-left" ng-submit="enviarFormulario('Editar')" id="formularioEditar" >
+         <form class="form-horizontal form-label-left" ng-submit="editarFormulario(abmConsultado.id)" id="formularioEditar" >
            <div class="item form-group">
              <label class="control-label col-md-3 col-sm-3 col-xs-12" for="nombre" >Nombre <span class="required">*</span>
              </label>
              <div class="col-md-6 col-sm-6 col-xs-12">
-               <input id="nombre" class="form-control col-md-7 col-xs-12" name="nombre" placeholder="Ingrese nombre del comercializador" type="text" ng-model="nombreComerc">{[{errores.nombre[0]}]}
+               <input  type="text" class="form-control col-md-7 col-xs-12" name="nombre" ng-model="abmConsultado.nombre">
              </div>
            </div>
            <div class="item form-group">
              <label class="control-label col-md-3 col-sm-3 col-xs-12" for="apellido">Apellido<span class="required">*</span>
              </label>
              <div class="col-md-6 col-sm-6 col-xs-12">
-               <input type="number" step="0.01" id="apellido" name="apellido" class="form-control col-md-7 col-xs-12" placeholder="Ingrese el apellido del comercializador" ng-model="apellidoComerc">{[{errores.cuota_social[0]}]}
+               <input type="text" step="0.01" id="apellido" name="apellido" class="form-control col-md-7 col-xs-12" placeholder="Ingrese el apellido del comercializador" ng-model="abmConsultado.apellido">{[{errores.cuota_social[0]}]}
              </div>
            </div>
 
@@ -264,51 +265,44 @@
              <label class="control-label col-md-3 col-sm-3 col-xs-12" for="cuit" >Cuit<span class="required">*</span>
              </label>
              <div class="col-md-6 col-sm-6 col-xs-12">
-               <input type="text" id="cuit" name="cuit" class="form-control col-md-7 col-xs-12" placeholder="Ingrese el cuit" ng-model="cuitComerc">{[{errores.cuit[0]}]}
+               <input type="text" id="cuit" name="cuit" class="form-control col-md-7 col-xs-12" placeholder="Ingrese el cuit" ng-model="abmConsultado.cuit">{[{errores.cuit[0]}]}
              </div>
            </div>
            <div class="item form-group">
            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="documento" >Documento<span class="required">*</span>
            </label>
            <div class="col-md-6 col-sm-6 col-xs-12">
-             <input type="text" id="documento" name="documento" class="form-control col-md-7 col-xs-12" placeholder="Ingrese el Documento" ng-model="documentoComerc">{[{errores.cuit[0]}]}
+             <input type="text" id="documento" name="documento" class="form-control col-md-7 col-xs-12" placeholder="Ingrese el Documento" ng-model="abmConsultado.dni">{[{errores.cuit[0]}]}
            </div>
          </div>
          <div class="item form-group">
          <label class="control-label col-md-3 col-sm-3 col-xs-12" for="documento" >Email<span class="required">*</span>
          </label>
          <div class="col-md-6 col-sm-6 col-xs-12">
-           <input type="text" id="email" name="email" class="form-control col-md-7 col-xs-12" placeholder="Ingrese el email" ng-model="emailComerc">{[{errores.cuit[0]}]}
+           <input type="text" id="email" name="email" class="form-control col-md-7 col-xs-12" placeholder="Ingrese el email" ng-model="abmConsultado.email">{[{errores.cuit[0]}]}
          </div>
        </div>
          <div class="item form-group">
          <label class="control-label col-md-3 col-sm-3 col-xs-12" for="cuit" >Domicilio<span class="required">*</span>
          </label>
          <div class="col-md-6 col-sm-6 col-xs-12">
-           <input type="text" id="domicilio" name="domicilio" class="form-control col-md-7 col-xs-12" placeholder="Ingrese el Domicilio" ng-model="domicilioComerc">{[{errores.cuit[0]}]}
+           <input type="text" id="domicilio" name="domicilio" class="form-control col-md-7 col-xs-12" placeholder="Ingrese el Domicilio" ng-model="abmConsultado.domicilio">{[{errores.cuit[0]}]}
          </div>
        </div>
        <div class="item form-group">
        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="cuit" >Telefono<span class="required">*</span>
        </label>
        <div class="col-md-6 col-sm-6 col-xs-12">
-         <input type="text" id="telefono" name="telefono" class="form-control col-md-7 col-xs-12" placeholder="Ingrese el telefono" ng-model="telefonoComerc">{[{errores.cuit[0]}]}
+         <input type="text" id="telefono" name="telefono" class="form-control col-md-7 col-xs-12" placeholder="Ingrese el telefono" ng-model="abmConsultado.telefono">{[{errores.cuit[0]}]}
        </div>
      </div>
      <div class="item form-group">
      <label class="control-label col-md-3 col-sm-3 col-xs-12" for="cuit" >Usuario<span class="required">*</span>
      </label>
      <div class="col-md-6 col-sm-6 col-xs-12">
-       <input type="text" id="usuario" name="usuario" class="form-control col-md-7 col-xs-12" placeholder="Ingrese el usuario" ng-model="usuarioComerc">{[{errores.cuit[0]}]}
+       <input type="text" id="usuario" name="usuario" class="form-control col-md-7 col-xs-12" placeholder="Ingrese el usuario" ng-model="abmConsultado.usuario">{[{errores.cuit[0]}]}
      </div>
    </div>
-   <div class="item form-group">
-   <label class="control-label col-md-3 col-sm-3 col-xs-12" for="cuit" >Contraseña<span class="required">*</span>
-   </label>
-   <div class="col-md-6 col-sm-6 col-xs-12">
-     <input type="text" id="contraseña" name="tcontraseña" class="form-control col-md-7 col-xs-12" placeholder="Ingrese la contraseña" ng-model="contraseniaComerc">{[{errores.cuit[0]}]}
-   </div>
- </div>
 
                     <input type="hidden" name="id">
                     <div class="ln_solid"></div>
@@ -323,6 +317,7 @@
       </div>
 
     </div>
+    @endverbatim
 
   </div>
 </div>
