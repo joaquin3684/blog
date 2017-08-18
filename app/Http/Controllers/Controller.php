9728402caf3256e6d15ea527874b7ccbbcd9d5c6
joaquin3684 <joaquinmazoud@gmail.com>
2017-08-18 12:12:09 -0300

    adsfds

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,26 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function unirColecciones($array1, $array2, $parametrosComparativo, $parametrosNulos = null)
+    {
+        $unido =  $array1->map(function ($item) use ($array2, $parametrosComparativo, $parametrosNulos){
+            $var = $array2->first(function ($item2) use ($item, $parametrosComparativo){
+                foreach ($parametrosComparativo as $parametro){
+                    if($item->$parametro != $item2->$parametro){
+                        return false;
+                    }
+                }
+                return true;
+            });
+            $item = collect($item);
+            if($var == null)
+            {
+                return $item->union($parametrosNulos);
+            }
+            return $item->union($var);
+        });
+        return $unido;
+
+    }
 }
