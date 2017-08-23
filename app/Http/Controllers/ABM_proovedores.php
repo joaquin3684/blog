365c2c7f5174f2f44fd69943f2dfed5e1271c114
joaquin3684 <joaquinmazoud@gmail.com>
2017-08-23 17:07:55 -0300

    flaksdjfa

--- app/Http/Controllers/ABM_proovedores.php
+++ app/Http/Controllers/ABM_proovedores.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
 use Illuminate\Http\Request;
 use App\Http\Requests\ValidacionABMproovedores;
 use Yajra\Datatables\Facades\Datatables;
