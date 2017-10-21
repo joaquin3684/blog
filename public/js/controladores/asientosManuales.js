ad72a579aee1a8d2ef6fa062d01b006a87b6bcdb
LucasBlanco <lucas_manuel_blanco@hotmail.com>
2017-10-21 10:28:01 -0300

    Agrego pantalla de asientos manuales para revision

---
+++ public/js/controladores/asientosManuales.js
@@ -0,0 +1,106 @@
+
+var app = angular.module('Mutual', ['ngMaterial', 'ngSanitize', 'ngTable', 'Mutual.services']).config(function($interpolateProvider) {});
+
+app.controller('asientosManuales', function($scope, $http, $compile, $sce, NgTableParams, $filter, UserSrv) {
+
+  // manda las solicitud http necesarias para manejar los requerimientos de un abm
+
+  $scope.borrarFormulario = function(){
+    $('#formulario')[0].reset();
+    $scope.asientos = [{
+      'id_imputacion': null,
+      'debe': null,
+      'haber': null,
+    }]
+  };
+
+
+  $scope.submit = function() {
+
+    var data = {
+      'asientos': $scope.asientos,
+    };
+
+    return $http({
+      url: 'asientos',
+      method: 'post',
+      data: data,
+
+    }).then(function successCallback(response) {
+      $scope.borrarFormulario();
+
+      UserSrv.MostrarMensaje("OK","Operación ejecutada correctamente.","OK","mensaje");
+      $scope.traerElementos();
+    }, function errorCallback(response) {
+      $scope.errores = response.data;
+    });
+
+  }
+
+  $scope.traerElementos = function() {
+
+    return $http({
+      url: "imputacion/traerElementos",
+      method: "get",
+    }).then(function successCallback(response) {
+        if (typeof response.data === 'string') {
+          return [];
+        } else {
+            console.log(response.data);
+            $scope.idDisponibles = response.data;
+          }
+
+
+      }, function errorCallback(response) {
+
+      });
+  }
+
+  $scope.traerElementos();
+
+$scope.sumarTotales = function (){
+  $scope.sumaDebe = 0;
+  $scope.sumaHaber = 0;
+  $scope.asientos.forEach(function(entry) {
+    $scope.sumaDebe += entry.debe;
+    $scope.sumaHaber += entry.haber;
+  });
+};
+$scope.verificarIgualdad = function(){
+
+  if($scope.sumaHaber == $scope.sumaDebe){
+    $scope.submit();
+  }
+  else{
+    UserSrv.MostrarMensaje("Error","La suma de los haber debe ser igual a la de los deber","Error","mensaje");
+  }
+};
+
+
+  $scope.asientos = [{
+    'id_imputacion': null,
+    'debe': null,
+    'haber': null,
+  }]
+  var cantComponentes = 1
+    $scope.agregarHtml = function(destino) {
+
+      destino.push({
+        'id_imputacion': null,
+        'debe': null,
+        'haber': null,
+      })
+
+  }
+
+  $scope.eliminarHtml = function (clon, array, indice){
+
+    if(array.length >1){
+      $(clon).remove();
+      array.splice(indice, 1);
+
+    }
+};
+
+
+});
---
+++ resources/views/asientosManuales.blade.php
@@ -0,0 +1,133 @@
+@extends('welcome')
+
+@section('contenido')
+
+
+{!! Html::script('js/controladores/asientosManuales.js') !!}
+
+
+<div class="nav-md" ng-controller="asientosManuales" >
+
+  <div class="container body">
+
+    <div class="main_container" >
+
+      <input type="hidden" id="tipo_tabla" value="organismos">
+      <!-- page content -->
+      <div class="left-col" role="main" >
+
+        <div class="" >
+
+          <div class="clearfix"></div>
+          <div id="mensaje"></div>
+          <div class="row" >
+            <div class="col-md-12 col-sm-12 col-xs-12" >
+              <div class="x_panel"  >
+                <div class="x_title">
+                  <h2>Formulario de asientos manuales </h2>
+                  <ul class="nav navbar-right panel_toolbox">
+                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                    </li>
+                    <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                      <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Settings 1</a>
+                        </li>
+                        <li><a href="#">Settings 2</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li><a class="close-link"><i class="fa fa-close"></i></a>
+                    </li>
+                  </ul>
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                  @verbatim
+                  <form class="form-horizontal form-label-left" id="formulario" ng-submit="verificarIgualdad()">
+                   {{ csrf_field() }}
+
+                   <table class="table">
+ <thead>
+   <tr>
+
+     <th style="text-align: center">Id. imputacion</th>
+     <th style="text-align: center">Debe</th>
+     <th style="text-align: center">Haber</th>
+     <th></th>
+   </tr>
+ </thead>
+ <tbody>
+
+     <tr ng-repeat="asiento in asientos" ng-attr-id="asiento{{$index}}">
+       <td style="border-top: 1px solid white;">
+           <select class="form-control" id="sel1" ng-model="asiento.id_imputacion">
+             <option value="id" ng-repeat="id in idDisponibles">{{id}}</option>
+           </select>
+       </td>
+       <td style="border-top: 1px solid white;">
+         <input type="number" name="" value="" class="form-control" ng-model="asiento.debe" placeholder="Ingrese el valor" ng-disabled="asiento.haber != null" >
+       </td>
+       <td style="border-top: 1px solid white;">
+         <input type="number" name="" value="" class="form-control" ng-model="asiento.haber" placeholder="Ingrese el haber" ng-disabled="asiento.debe != null">
+       </td>
+       <td style="border-top: 1px solid white;">
+         <button id="sumahtml" type="button" class="btn btn-danger"  ng-click="eliminarHtml('#asiento'+$index, asientos,$index)">
+         <span class="glyphicon glyphicon-minus" aria-hidden="true" ></span>
+       </button>
+     <button id="sumahtml" type="button" class="btn btn-primary"  ng-click="agregarHtml(asientos)">
+       <span class="glyphicon glyphicon-plus" aria-hidden="true" ></span>
+     </button>
+      </td>
+     </tr>
+
+ </tbody>
+    <tfoot>
+      <tr style="font-size: initial">
+        <th>Totales:</th>
+        {{sumarTotales()}}
+        <th>{{sumaDebe}}</th>
+        <th>{{sumaHaber}}</th>
+        <th></th>
+      </tr>
+    </tfoot>
+</table>
+                    <div class="ln_solid"></div>
+                    <div class="form-group">
+                      <div class="col-md-6 col-md-offset-3">
+                        <button type="button" ng-click="borrarFormulario()" class="btn btn-primary">Cancelar</button>
+                        <button id="send" type="submit" class="btn btn-success">Alta</button>
+                      </div>
+                    </div>
+                  </form>
+                  @endverbatim
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
+
+
+      <!-- /page content -->
+    </div>
+
+  </div>
+
+  <div id="custom_notifications" class="custom-notifications dsp_none">
+    <ul class="list-unstyled notifications clearfix" data-tabbed_notifications="notif-group">
+    </ul>
+    <div class="clearfix"></div>
+    <div id="notif-group" class="tabbed_notifications"></div>
+  </div>
+
+
+
+</div>
+
+
+@endsection
