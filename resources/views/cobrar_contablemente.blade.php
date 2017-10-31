b1e4ffdc9eb25a28e4ef08832b25787d506d5f80
vagrant <vagrant@homestead>
2017-10-31 16:01:07 +0000

    Cambio titulo que me falto en el commit anterior

--- resources/views/cobrar_contablemente.blade.php
+++ resources/views/cobrar_contablemente.blade.php
@@ -24,7 +24,7 @@
             <div class="col-md-12 col-sm-12 col-xs-12" >
               <div class="x_panel"  >
                 <div class="x_title">
-                  <h2>Formulario de bancos</h2>
+                  <h2>Cobrar contablemente</h2>
                   <ul class="nav navbar-right panel_toolbox">
                     <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                     </li>
