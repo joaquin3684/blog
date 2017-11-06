379e4b8c85133126543f0a2c09d557eaac36c0ec
vagrant <vagrant@homestead>
2017-11-06 21:41:48 +0000

    ajosdfaj

---
+++ app/Repositories/Eloquent/Contabilidad/GeneradorDeCuentas.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joaquin
+ * Date: 06/11/17
+ * Time: 18:13
+ */
+
+namespace App\Repositories\Eloquent\Contabilidad;
+
+
+use App\Imputacion;
+use App\Repositories\Eloquent\ControlFechaContable;
+use App\SaldosCuentas;
+use App\SubRubro;
+
+class GeneradorDeCuentas
+{
+    public static function generar($nombre, $codigo)
+    {
+        $fechaOperativa = new ControlFechaContable();
+        $fecha = $fechaOperativa->fechaContable;
+        $codigoSubRubro = substr((string) $codigo, 0, -2);
+        $subRubro = SubRubro::where('codigo', $codigoSubRubro)->first();
+        $imputacion = Imputacion::create(['nombre' => $nombre, 'codigo' => $codigo, 'id_subrubro' => $subRubro->id]);
+        SaldosCuentas::create(['saldo' => 0, 'year' => $fecha->year, 'month' => $fecha->month, 'codigo' => $imputacion->codigo, 'id_imputacion' => $imputacion->id]);
+    }
+}
