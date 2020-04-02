3a458f53ee7bccb75c4b1c9e6f4f06eb1571c629
LucasBlanco <lucas_manuel_blanco@hotmail.com>
2020-04-02 12:03:59 -0300

    se muestra la suma de los montos a cobrar de los comercializadores correctamente

--- public/js/controladores/solicitudesPendientesDeCobro.js
+++ public/js/controladores/solicitudesPendientesDeCobro.js
@@ -1,28 +1,28 @@
-var app = angular.module('Mutual').config(function ($interpolateProvider) { });
+var app = angular.module('Mutual').config(function ($interpolateProvider) {});
 app.requires.push('ngMaterial', 'ngSanitize', 'ngTable', 'ServicioABM', 'Mutual.services', 'angular-loading-bar');
 app.controller('solicitudesPendientesDeCobro', function ($scope, $http, $compile, $sce, NgTableParams, $filter, ServicioABM) {
 
     $scope.borrarFormulario = function () {
         $('#formulario2')[0].reset();
-  
+
     };
 
 
     var pull = function (url, scopeObj, params) {
-        moment.locale('es')
+        moment.locale('es');
         ServicioABM.pull(url).then(function (returnedData) {
             $scope[scopeObj] = returnedData.map(solicitud => solicitud.updated_at = moment(solicitud.updated_at).format('L'));
-            $scope.sumaTotalACobrar = $scope.solicitudes.map(el => el.montoACobrar).reduce((acum, monto) => acum + monto)
-            $scope[params] = ServicioABM.createTable(returnedData)
+            $scope.sumaTotalACobrar = returnedData.map(el => Number(el.montoACobrar)).reduce((acum, monto) => acum + monto, 0);
+            $scope[params] = ServicioABM.createTable(returnedData);
         });
-    }
+    };
     var pullElem = function (url, scopeObj, id) {
         ServicioABM.pull(url, id).then(function (returnedData) {
             $scope[scopeObj] = returnedData;
         });
-    }
+    };
 
-    
+
     pull('solicitudesPendientesDeCobro/solicitudes', 'solicitudes', 'paramsSolicitud');
-    
+
 });
